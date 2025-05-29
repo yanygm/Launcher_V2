f93c0cdbca28cb4e1d33e57f9d3bded1f9f84ca6
Yany <475016817@msn.cn>
2025-05-29 14:23:12 +0800

    Update GameType.cs

--- KartRider.Data/GameType.cs
+++ KartRider.Data/GameType.cs
@@ -7,7 +7,6 @@ namespace KartRider
 		public static uint TimeAttack_RP = 0;
 		public static uint TimeAttack_Lucci = 0;
 		public static byte RewardType = 0;
-		public static byte TimeAttackLevel = 0;
 
 		//Time
 		public static int min = 0;
