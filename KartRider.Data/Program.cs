93b636e57adbc9c3f6c9fa009378c24e4d25a1a2
Yany <475016817@msn.cn>
2025-08-16 09:55:20 +0800

    Update Program.cs

--- KartRider.Data/Program.cs
+++ KartRider.Data/Program.cs
@@ -28,7 +28,15 @@ namespace KartRider
 
         [DllImport("user32.dll")]
         public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
-        const int SW_HIDE = 0;
+
+        [DllImport("user32.dll")]
+        public static extern bool IsWindowVisible(IntPtr hWnd);
+
+        public const int SW_HIDE = 0;
+        public const int SW_SHOW = 5;
+        public static int consoleStatus = SW_SHOW;
+        public static string Load_Console = AppDomain.CurrentDomain.BaseDirectory + "Profile\\Console.ini";
+        public static IntPtr consoleHandle;
         public static Launcher LauncherDlg;
         public static GetKart GetKartDlg;
         public static bool SpeedPatch;
@@ -103,8 +111,7 @@ namespace KartRider
                         {
                             Console.WriteLine($"读取Data文件时出错: {ex.Message}");
                         }
-                        string Load_Console = AppDomain.CurrentDomain.BaseDirectory + "Profile\\Console.ini";
-                        IntPtr consoleHandle = System.Diagnostics.Process.GetCurrentProcess().MainWindowHandle;
+                        consoleHandle  = System.Diagnostics.Process.GetCurrentProcess().MainWindowHandle;
                         if (!File.Exists(Load_Console))
                         {
                             using (StreamWriter streamWriter = new StreamWriter(Load_Console, false))
