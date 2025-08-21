53f0dad0f8cb34826b4f4f535c2aa918ab6acdf5
Yany <475016817@msn.cn>
2025-08-21 13:12:24 +0800

    Update LauncherSystem.cs

--- KartRider.Data/LauncherSystem.cs
+++ KartRider.Data/LauncherSystem.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Windows.Forms;
 
 namespace KartRider
@@ -7,17 +7,18 @@ namespace KartRider
 	{
 		public static void MessageBoxType1()
 		{
-			MessageBox.Show("跑跑卡丁车已经运行了！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Information);
+			MessageBox.Show("跑跑卡丁车已经运行了！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
 		}
 
 		public static void MessageBoxType2()
 		{
-			MessageBox.Show("跑跑卡丁车已经运行了！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Information);
+			MessageBox.Show("已经有一个启动器在运行了！\n不可以同时运行多个启动器！\n点击确认退出程序", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			Environment.Exit(1);
 		}
 
 		public static void MessageBoxType3()
 		{
-			MessageBox.Show(Launcher.KartRider + " 或 " + Launcher.pinFile + " 找不到文件！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Information);
+			MessageBox.Show(Launcher.KartRider + " 或 " + Launcher.pinFile + " 找不到文件！\n点击确认退出程序", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
 			Environment.Exit(1);
 		}
 	}
