387890c3949abaf617489fc82403edea90f554c8
Yany <475016817@msn.cn>
2026-06-28 12:38:42 +0800

    Update ModelMax.cs

--- KartRider.Data/Profile/ModelMax.cs
+++ KartRider.Data/Profile/ModelMax.cs
@@ -23,24 +23,39 @@ public static class ModelMax
     <kart id='1515' name='怪物 V1' modelMaxX='0.79099' modelMaxY='1.10517' />
     <kart id='1523' name='仙界四不像 迅' modelMaxX='0.773709' modelMaxY='1.29294' />
     <kart id='1528' name='爆烈 迅' modelMaxX='0.8003274' modelMaxY='1.049711' />
-    <kart id='1530' name='精英爆烈 迅' modelMaxX='0.8003274' modelMaxY='1.049711' />
-    <kart id='1531' name='雷霆爆烈 迅' modelMaxX='0.8003274' modelMaxY='1.049711' />
+    <kart id='1529' name='高级爆烈 迅' modelMaxX='0.8003274' modelMaxY='1.049711' />
+    <kart id='1530' name='精英爆烈 迅' modelMaxX='0.7924033' modelMaxY='1.049711' />
+    <kart id='1531' name='雷霆爆烈 迅' modelMaxX='0.7924033' modelMaxY='1.049711' />
     <kart id='1532' name='合金 迅' modelMaxX='0.8232' modelMaxY='1.041413' />
+    <kart id='1533' name='高级合金 迅' modelMaxX='0.8232' modelMaxY='1.041413' />
     <kart id='1534' name='精英合金 迅' modelMaxX='0.8134' modelMaxY='1.030677' />
     <kart id='1535' name='钢铁合金 迅' modelMaxX='0.8134' modelMaxY='1.030677' />
     <kart id='1537' name='野马敞篷跑车 迅' modelMaxX='0.773879' modelMaxY='1.095391' />
     <kart id='1538' name='鲲鹏之势 迅' modelMaxX='0.831644' modelMaxY='1.123067' />
     <kart id='1539' name='马拉松 迅' modelMaxX='0.8439705' modelMaxY='0.963268' />
-    <kart id='1541' name='精英马拉松 迅' modelMaxX='0.8439705' modelMaxY='0.963268' />
+    <kart id='1540' name='高级马拉松 迅' modelMaxX='0.8439705' modelMaxY='0.963268' />
+    <kart id='1541' name='精英马拉松 迅' modelMaxX='0.8355308' modelMaxY='0.9536353' />
     <kart id='1542' name='烈焰马拉松 迅' modelMaxX='0.8355308' modelMaxY='0.9536353' />
     <kart id='1544' name='尖锋 迅' modelMaxX='0.77347' modelMaxY='0.981104' />
+    <kart id='1545' name='高级尖锋 迅' modelMaxX='0.77347' modelMaxY='0.981104' />
     <kart id='1546' name='精英尖锋 迅' modelMaxX='0.765735' modelMaxY='0.971293' />
     <kart id='1547' name='圣光天启尖锋 迅' modelMaxX='0.765735' modelMaxY='0.971293' />
     <kart id='1549' name='先驱者 迅' modelMaxX='0.765396' modelMaxY='1.03144' />
+    <kart id='1550' name='离子 迅' modelMaxX='0.771933' modelMaxY='1.153993' />
+    <kart id='1552' name='离子 迅 黑炎' modelMaxX='0.761359' modelMaxY='1.153993' />
+    <kart id='1553' name='X10 暗黑 迅' modelMaxX='0.8232' modelMaxY='1.041413' />
+    <kart id='1556' name='皮蛋计划 迅' modelMaxX='0.7633' modelMaxY='1.0304' />
+    <kart id='1560' name='宝宝计划 迅' modelMaxX='0.7624' modelMaxY='0.9811' />
     <kart id='1562' name='魔界四不像 迅' modelMaxX='0.773709' modelMaxY='1.29294' />
     <kart id='1564' name='诛仙四剑 迅' modelMaxX='0.77347' modelMaxY='0.981104' />
+    <kart id='1568' name='鹊桥飞光 迅' modelMaxX='0.7837' modelMaxY='1.2089' />
+    <kart id='1572' name='小强计划 迅' modelMaxX='0.7734' modelMaxY='0.9811' />
     <kart id='1574' name='黑骑士 迅' modelMaxX='0.7339' modelMaxY='1.1108' />
+    <kart id='1583' name='赤炎威龙 迅' modelMaxX='0.7599165' modelMaxY='1.323392' />
     <kart id='1587' name='疾影 迅' modelMaxX='0.720975' modelMaxY='0.969122' />
+    <kart id='1589' name='冰橇 迅' modelMaxX='0.744729' modelMaxY='1.053629' />
+    <kart id='1596' name='黄金威龙 迅' modelMaxX='0.7599165' modelMaxY='1.323392' />
+    <kart id='1598' name='小乖计划 迅' modelMaxX='0.8005' modelMaxY='0.8335' />
     <kart id='1614' name='R8 迅' modelMaxX='0.744088' modelMaxY='1.150446' />
     <kart id='1616' name='等离子 迅' modelMaxX='0.780103' modelMaxY='1.139165' />
     <kart id='1617' name='高级等离子 迅' modelMaxX='0.780103' modelMaxY='1.139165' />
